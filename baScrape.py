4b1e494e40556e44ee2b1d773f71ea3da68b516d
codeNIH <codenih@gmail.com>
2016-05-03 15:10:27 -0400

    Update baScrape.py

--- baScrape.py
+++ baScrape.py
@@ -14,14 +14,24 @@ driver = webdriver.Firefox()
 driver.get("http://www.ipilab.org/BAAweb/")
 
 # Dropdown menu attributes, according to source code
-raceID = ["ASI", "BLK", "CAU", "HIS"]
-genderID = ["F", "M"]
-ageID = ["00", "01", "02", "03", "04", "05", "06", "07", 
+raceName = "Race"
+genderName = "Gender"
+ageName = "Age"
+
+raceVal = ["ASI", "BLK", "CAU", "HIS"]
+genderVal = ["F", "M"]
+ageVal = ["00", "01", "02", "03", "04", "05", "06", "07", 
 		"08", "09", "10", "11", "12", "13", "14", "15", 
 		"16", "17", "18"]
 
-for race in raceID:
+for race in raceVal:
 	if race == "CAU":
-		driver.find_element_by_xpath('//select[@name=' + race + ']' + '/option[@value=')
-	driver.find_element_by_xpath('//select[@name=' + race + ']' + '/option[@value=')
+		driver.find_element_by_xpath('//select[@name=' + raceName + ']/option[@vaule=' + raceVal + ']').click()
+	else:
+		driver.find_element_by_xpath('//select[@name=' + raceName + ']/option[@value=' + raceVal + ']').click()
+	for gender in genderVal:
+		driver.find_element_by_xpath('//select[@name=' + genderName + ']/option[@value=' + genderVal + ']').click()
+		for age in ageVal:
+			driver.find_element_by_xpath('//select[@name=' + ageName + ']/option[@value=' + ageVal + ']').click()
+			
 
