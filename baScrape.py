e7bd8aa54d458ea77060209e85e39a3d6b95aea0
Alex <codenih@gmail.com>
2016-05-04 21:54:53 -0400

    updated baScrape

--- baScrape.py
+++ baScrape.py
@@ -18,20 +18,40 @@ raceName = "Race"
 genderName = "Gender"
 ageName = "Age"
 
-raceVal = ["ASI", "BLK", "CAU", "HIS"]
-genderVal = ["F", "M"]
-ageVal = ["00", "01", "02", "03", "04", "05", "06", "07", 
-		"08", "09", "10", "11", "12", "13", "14", "15", 
-		"16", "17", "18"]
+raceVal = ['"ASI"', '"BLK"', '"CAU"', '"HIS"']
+genderVal = ['"F"', '"M"']
+ageVal = ['"00"', '"01"', '"02"', '"03"', '"04"', '"05"', '"06"', '"07"', 
+		'"08"', '"09"', '"10"', '"11"', '"12"', '"13"', '"14"', '"15"', 
+		'"16"', '"17"', '"18"']
 
+# Iterate all dropdown attribute combinations (i.e BLK FEMALE 12)
+
+# Race iteration
 for race in raceVal:
+	# **There is an HTML misspelling which causes value to be spelled vaule**
 	if race == "CAU":
-		driver.find_element_by_xpath('//select[@name=' + raceName + ']/option[@vaule=' + raceVal + ']').click()
+		driver.find_element_by_xpath('//select[@name="Race"]/option[@vaule=' + race + ']').click()
 	else:
-		driver.find_element_by_xpath('//select[@name=' + raceName + ']/option[@value=' + raceVal + ']').click()
+		driver.find_element_by_xpath('//select[@name="Race"]/option[@value=' + race + ']').click()
+	# Gender iteration
 	for gender in genderVal:
-		driver.find_element_by_xpath('//select[@name=' + genderName + ']/option[@value=' + genderVal + ']').click()
+		driver.find_element_by_xpath('//select[@name="Gender"]/option[@value=' + gender + ']').click()
+		# Age iteration
 		for age in ageVal:
-			driver.find_element_by_xpath('//select[@name=' + ageName + ']/option[@value=' + ageVal + ']').click()
+			driver.find_element_by_xpath('//select[@name="Age"]/option[@value=' + age + ']').click()
+			time.sleep(1)
+			driver.find_element_by_xpath('//input[@value="Submit"]').click()
+			htmlSource = driver.page_source
+			soupObject = BeautifulSoup(htmlSource, 'html.parser')
+			# Find all image links and download
+			for item in soupObject.findAll('a'):
+				rawLink = link.get('href')[:-3]
+				landmark = rawLink.index("JPEG")
+				linkURL = rawLink[landmark:]
+				NAME = linkURL.rpartition('/')[0].rpartition('/')[2]
+				UID = linkURL.rpartition('/')[2][:4]
+				#print(link.get('href')[:-3])
+
+
 			
 
