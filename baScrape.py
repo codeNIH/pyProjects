3bcd6df9c92d4020f6b2d9170dde22abc16195c2
Alex George <alexjgeorge636@gmail.com>
2016-05-18 19:16:33 -0400

    made meta changes

--- baScrape.py
+++ baScrape.py
@@ -27,9 +27,10 @@ ageName = "Age"
 # 		'"08"', '"09"', '"10"', '"11"', '"12"', '"13"', '"14"', '"15"', 
 # 		'"16"', '"17"', '"18"']
 
-raceVal = ['"ASI"', '"BLK"']
-genderVal = ['"F"', '"M"']
-ageVal = ['"00"', '"01"', '"02"', '"03"', '"04"']
+raceVal = ['"BLK"']
+genderVal = ['"F"']
+#ageVal = ['"00"', '"01"', '"02"', '"03"', '"04"']
+ageVal = ['"00"']
 
 # Iterate all dropdown attribute combinations (i.e BLK FEMALE 12)
 
@@ -46,10 +47,18 @@ for race in raceVal:
 		# Age iteration
 		for age in ageVal:
 			driver.find_element_by_xpath('//select[@name="Age"]/option[@value=' + age + ']').click()
-			time.sleep(1)
+			#time.sleep(1)
 			driver.find_element_by_xpath('//input[@value="Submit"]').click()
 			htmlSource = driver.page_source
 			soupObject = BeautifulSoup(htmlSource, 'html.parser')
+
+			metaData = []
+
+			for row in soupObject.find_all("td"):
+				item = row.getText()
+				metaData.append(item)
+			
+			i = 0
 			# Find all image links and download
 			for link in soupObject.findAll('a'):
 				rawLink = link.get('href')[:-3]
@@ -67,10 +76,29 @@ for race in raceVal:
 				elif race == '"HIS"':
 					destPATH = '/Users/alexgeorge/Dropbox/Python/codeNIH/Images/HIS/'
 
-				urllib.urlretrieve(PATH+linkURL, destPATH+ NAME + '_' + UID + '.jpg')
+				metaID = UID
+				metaRace = "Race: " + str(metaData[1 + i*12])
+				metaGender = "Gender: " + str(metaData[2 + i*12])
+				metChr = "Chronological Age: " + str(metaData[3 + i*12])
+				metaDOB = "DOB: " + str(metaData[4 + i*12])
+				metaExam = "Exam Date: " + str(metaData[5 + i*12])
+				metaTanner = "Tanner: " + str(metaData[6 + i*12])
+				metaHeight = "Height (cm): " + str(metaData[7 + i*12])
+				metaWeight = "Weight (kg): " + str(metaData[8 + i*12])
+				metaTrunk = "Trunk Height (cm): " + str(metaData[9 + i*12])
+				metaRead1 = "Reading 1: " + str(metaData[10 + i*12])
+				metaRead2 = "Reading 2: " + str(metaData[11 + i*12])
+				
 
-				print NAME+UID
-				#print(link.get('href')[:-3])
+				#urllib.urlretrieve(PATH+linkURL, destPATH+ NAME + '_' + UID + '.jpg')
+				i += 1
+
+			print '\n'
+			# for item in metaData:
+			# 	print item
+
+
+driver.quit()
 
 
 			
