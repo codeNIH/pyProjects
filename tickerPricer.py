1579960db2bc46ca35b68161b223d25453c1e202
codeNIH <codenih@gmail.com>
2016-10-16 12:01:23 -0400

    Created file

---
+++ tickerPricer.py
@@ -0,0 +1,5 @@
+# Objective A: Prompt user to enter a ticker symbol
+# Objective B: Route to appropriate Yahoo Finance url
+# Objective C: Scrape site and find CURRENT price/share
+# Objective D: Return price on console to user
+
